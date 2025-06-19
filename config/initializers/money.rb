5d5578d51d2524f82dd26d4953fce64ddcf6d252
Von Christian Halip <vc.halip@gmail.com>
2025-06-19 13:48:34 +0800

    Setup line items model

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,10 +1,9 @@
 # encoding : utf-8
 
 MoneyRails.configure do |config|
-
   # To set the default currency
   #
-  # config.default_currency = :usd
+  config.default_currency = :usd
 
   # Set default bank object
   #
@@ -85,11 +84,11 @@ MoneyRails.configure do |config|
 
   # If you would like to use I18n localization (formatting depends on the
   # locale):
-  # config.locale_backend = :i18n
+  config.locale_backend = :i18n
   #
   # Example (using default localization from rails-i18n):
   #
-  # I18n.locale = :en
+  I18n.locale = :en
   # Money.new(10_000_00, 'USD').format # => $10,000.00
   # I18n.locale = :es
   # Money.new(10_000_00, 'USD').format # => $10.000,00
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  describe 'associations' do
+    it { should have_many(:line_items).dependent(:destroy) }
+  end
 end
