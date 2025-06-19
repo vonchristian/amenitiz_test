904d65c211b87a0315a2adb4d328f5c85716c906
Von Christian Halip <vc.halip@gmail.com>
2025-06-19 15:31:29 +0800

    Add coffee product to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,18 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
+# Products
 green_tea = Product.find_or_create_by!(name: 'Green Tea', code: 'GR1')
-green_tea_price = Price.create!(product: green_tea, amount_cents: 311, amount_currency: 'GBP', active: true)
 strawberry = Product.find_or_create_by!(name: 'Strawberry', code: 'SR1')
+coffee = Product.find_or_create_by!(name: 'Coffee', code: 'CF1')
+# Prices
+green_tea_price = Price.create!(product: green_tea, amount_cents: 311, amount_currency: 'GBP', active: true)
 strawberry_price = Price.create!(product: strawberry, amount_cents: 500, amount_currency: 'GBP', active: true)
+coffee_price = Price.create!(product: coffee, amount_cents: 1123, amount_currency: 'GBP', active: true)
+# Promo Rules
 bulk_discount_strawberry = PromoRules::BulkDiscountRule.find_or_create_by!(min_quantity: 3, discount_price_cents: 450, discount_price_currency: 'GBP')
+percent_discount_rule = PromoRules::PercentDiscountRule.find_or_create_by!(min_quantity: 3, discount_percent: 33.33333)
+# Promotions
 Promotion.find_or_create_by!(code: 'GR1_PROMO', name: 'Green Tea Promotion', rule_type: 'PromoRules::BuyOneGetOneRule', product: green_tea, active: true)
 Promotion.find_or_create_by!(code: 'SR1_PROMO', name: 'Strawberry Promotion', rule: bulk_discount_strawberry, product: strawberry, active: true)
+Promotion.find_or_create_by!(code: 'CF1_PROMO', name: 'Coffee Promotion', rule: percent_discount_rule, product: coffee, active: true)
